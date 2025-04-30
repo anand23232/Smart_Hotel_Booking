1c99646601ca9964c9457ba7c937cdd0deaed912
Yeramala Dherandar Reddy <dherandarreddyyeramala@gmail.com>
2025-04-30 21:51:42 +0530

    "Backend Structure Completed"

--- SmartHotelBookingSystem/src/main/java/com/cts/SmartHotelBookingSystem/service/RazorpayService.java
+++ SmartHotelBookingSystem/src/main/java/com/cts/SmartHotelBookingSystem/service/RazorpayService.java
@@ -1,9 +1,9 @@
 package com.cts.SmartHotelBookingSystem.service;
- 
+  
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
- 
+
 import com.razorpay.Order;
 import com.razorpay.RazorpayClient;
 import com.razorpay.RazorpayException;
@@ -17,12 +17,12 @@ public class RazorpayService {
     @Value("${razorpay.api.secret}")
     private String apiSecret ;
  
-    public String createOrder(int amount , String currency , String receipt) throws RazorpayException {
+    public String createOrder(int amount , String currency , String receiptId) throws RazorpayException {
         RazorpayClient razorpayClient = new RazorpayClient(apiKey,apiSecret);
         JSONObject orderRequest  = new JSONObject();
         orderRequest.put("amount", amount *100);
         orderRequest.put("currency",currency);
-        orderRequest.put("receipt", receipt);
+        orderRequest.put("receipt", receiptId);
  
         Order order = razorpayClient.orders.create(orderRequest);
         return order.toString();
